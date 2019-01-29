b5d1d6bbfa3f4ea77b808a1892a125ed8bc981f8
Danielle Martin <DanielleM565@users.noreply.github.com>
2019-01-28 18:17:42 -0700

    What I have so far 1-28
    https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/adding-model?view=aspnetcore-2.0&tabs=visual-studio
    
    working on add a model

---
+++ web-task-1-movies-DanielleM565/movies/movies/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace movies
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
+//MVC
+//Models - classes that represent the data of the app
+//views - components that display the User interphase
+//Controllers - classes that handle browser requests. handel calls and responses
